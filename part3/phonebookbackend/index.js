3503edd9ab7b57bc43bc0b4d4e6bd67085a72f84
Javier Rosales <javieralberto0912@gmail.com>
2023-02-16 12:13:35 +0200

    3.4

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -38,10 +38,10 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    if(notes[request.params.id])
+    if(findValue(request.params.id))
     {        
         response.writeHead(200, { 'Content-Type': 'application/json' })
-        response.end(JSON.stringify(notes[request.params.id]))
+        response.end(JSON.stringify(findValue(id)))
     }
     else {        
         response.writeHead(404, { 'Content-Type': 'text/plain' })
@@ -50,6 +50,31 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    if(findValue(request.params.id))
+    {
+        delete notes[notes.indexOf(findValue(request.params.id))]  
+        response.writeHead(200, { 'Content-Type': 'application/json' })
+        response.end('Contanct delted new phonebook: '+JSON.stringify(notes))
+        
+    }
+    else {        
+        response.writeHead(404, { 'Content-Type': 'text/plain' })
+        response.end(` Phonebook has  no info for id ${request.params.id} \n 
+        ${(new Date(Date.now())).toUTCString()}`)
+    }
+})
+
+const findValue = (id) => {
+    console.log(id)
+    for (let index = 0; index < notes.length; index++) {
+        const element = notes[index];
+        if(element.id == id){
+            return element
+        }
+    }
+    return false
+}
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
