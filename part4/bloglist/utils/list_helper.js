e025c07e274daab5ddc0b5fe4e81dbc6f93ee9aa
Javier Rosales <javieralberto0912@gmail.com>
2023-03-07 19:48:25 +0200

    4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,11 @@
 const dummy = (blogs) => {return 1}
 
-module.exports = {dummy,}
+const totalLikes = (blogList) => {
+    let totalLikes = 0;
+    blogList.forEach(element => {
+        totalLikes = totalLikes + element.likes
+    });
+    return totalLikes
+}
+
+module.exports = {dummy, totalLikes}
