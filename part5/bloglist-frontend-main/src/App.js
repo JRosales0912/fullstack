c8ac9e77348600df92f54a6935b61903501f7ba0
Javier Rosales <javieralberto0912@gmail.com>
2023-03-16 17:51:47 +0200

    5.2

--- part5/bloglist-frontend-main/src/App.js
+++ part5/bloglist-frontend-main/src/App.js
@@ -58,6 +58,13 @@ const App = () => {
     } 
   }
 
+  function handleLogout(event) {    
+    event.preventDefault()       
+      setUser(null)
+      setUsername('')
+      setPassword('')
+  }
+
   if (user === null) {
     return (
       <div>
@@ -90,6 +97,7 @@ const App = () => {
     <div>
       <h2>blogs</h2>
       <h4> {user} logged in</h4>
+      <button onClick={handleLogout}>Logout</button>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
