29d4a58623fe52defb653ee84aed517c3d7272a5
Javier Rosales <javieralberto0912@gmail.com>
2023-05-22 13:04:24 +0300

    5.7

--- part5/bloglist-frontend-main/src/components/Blog.js
+++ part5/bloglist-frontend-main/src/components/Blog.js
@@ -1,7 +1,22 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false)
+
+  if(visible) {
+    return(
+      <div style={{border: '1.8px solid red'}}>
+      <h4>{blog.title} <button onClick={() => setVisible(false)}>View</button></h4> 
+      <h4>Author: {blog.author}</h4> 
+      <h5>URL: {blog.url}</h5> 
+      <h5>Likes: {blog.likes} <button>Like</button></h5>
+      </div>)
+  } else {
+    return(
+    <div style={{border: '1.5px solid blue'}}>
+    <h4>{blog.title} <button onClick={() => setVisible(true)}>View</button></h4> 
+    </div>
+    )
+  }    
+}
 
 export default Blog
