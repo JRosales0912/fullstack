04fee33f4552a42547000c3447388b7b854f6cea
Javier Rosales <javieralberto0912@gmail.com>
2023-06-05 14:47:25 +0300

    6.15

--- part6/redux-anecdotes-main/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes-main/src/components/AnecdoteForm.js
@@ -1,16 +1,17 @@
 import { useDispatch } from "react-redux"
 import { createAnecdote as create } from '../reducers/anecdoteReducer'
 import { hideNotification, showNotification } from "../reducers/notificationReducer"
-
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = (props) => {
     const dispatch = useDispatch()
 
-    const add = (event) => {
+    const add = async (event) => {
         event.preventDefault()
         const content = event.target.anecdote.value
         event.target.anecdote.value = ''
-        dispatch(create(content))
+        const newAnecdote = await anecdoteService.createNew(content)
+        dispatch(create(newAnecdote))
         dispatch(showNotification(`you created a new anecdote '${content}'`))
         setTimeout(() => {
             dispatch(hideNotification())
